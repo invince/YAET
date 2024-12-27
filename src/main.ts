69060db0acd475c530ec8257bcab49f375eabbd7
invince <invincezero@gmail.com>
2024-12-27 15:35:10 +0100

    update syncfusion key

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app/app.component';
 import { registerLicense } from '@syncfusion/ej2-base';
 
 // Registering Syncfusion license key
-registerLicense('Ngo9BigBOggjHTQxAR8/V1NMaF5cXmBCf1FpRmJGfV5ycEVFallWTnJYUj0eQnxTdEFiW31acXFQQWRbUEJ1Xg==');
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NMaF5cXmBCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWH1cd3RcRWNeU0Z+V0Q=');
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
