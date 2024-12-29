8367589d4cba71e114ee00ef188bebcd1c2d4395
invince <invincezero@gmail.com>
2024-12-29 17:43:42 +0100

    scp rename #88 scp copy paste #85 Scp cut paste #86

--- src-electron/ipc/scp.js
+++ src-electron/ipc/scp.js
@@ -2,7 +2,7 @@ const { ipcMain , dialog } = require('electron');
 const SftpClient = require('ssh2-sftp-client');
 const multer = require('multer');
 const upload = multer();
-
+const path = require('path');
 
 function initScpSftpHandler(scpMap, expressApp) {
 
@@ -10,8 +10,12 @@ function initScpSftpHandler(scpMap, expressApp) {
     scpMap.set(id, config);
   });
 
+
+
   expressApp.post('/list/:id', async (req, res) => {
-    const path = req.body.path || '/';
+    const action = req.body.action || 'read';
+    const pathParam = req.body.path || '/';
+
     const configId = req.params['id'];
     const config = scpMap.get(configId);
     if (!config) {
@@ -21,23 +25,142 @@ function initScpSftpHandler(scpMap, expressApp) {
     try {
       const sftp = new SftpClient();
       await sftp.connect(config);
-      const files = await sftp.list(path);
+      switch (action) {
+        case 'read': {
+          const files = await sftp.list(pathParam);
 
-      const formattedFiles = files.map(file => ({
-        name: file.name,
-        type: file.type === 'd' ? 'folder' : 'file',
-        isFile: file.type !== 'd',
-        size: file.size,
-        dateModified: file.modifyTime,
-      }));
+          const formattedFiles = files.map(file => ({
+            name: file.name,
+            type: file.type === 'd' ? 'folder' : 'file',
+            isFile: file.type !== 'd',
+            size: file.size,
+            dateModified: file.modifyTime,
+          }));
 
-      res.json({ cwd: { name: path, type: 'folder' }, files: formattedFiles });
+          res.json({ cwd: { name: pathParam, type: 'folder' }, files: formattedFiles });
+          break;
+        }
+        case 'delete': {
+          const names = req.body.names || [];
+          for (let i = 0; i < names.length; i++) {
+            const name = names[i];
+            const fileAbsPath = `${pathParam}${name}`;
+            await sftp.delete(fileAbsPath, true);
+          }
+          res.json({ cwd: { name: pathParam, type: 'folder' }});
+          break;
+        }
+        case 'rename': {
+          const name = req.body.name;
+          const newName = req.body.newName;
+          await sftp.rename(`${pathParam}${name}`, `${pathParam}${newName}`);
+          res.json({ cwd: { name: pathParam, type: 'folder' }, files: getDetails(sftp, pathParam, newName) });
+          break;
+        }
+        case 'copy': {
+          const names = req.body.names || [];
+          const targetPath = req.body.targetPath;
+          for (const name of names) {
+            const sourceFilePath = `${pathParam}${name}`;
+            const targetFilePathOg = `${targetPath}${name}`;
+            const targetFilePath = await avoidDuplicateName(sftp, targetFilePathOg);
+            // Copy file
+            await sftp.rcopy(sourceFilePath, targetFilePath)
+            res.json({ cwd: { name: pathParam, type: 'folder' }, files: getDetails(sftp, targetPath, names) });
+          }
+          break;
+        }
+        case 'move': {
+          const names = req.body.names || [];
+          const targetPath = req.body.targetPath;
+          if (targetPath === pathParam) {
+            break;
+          }
+          for (const name of names) {
+            const sourceFilePath = `${pathParam}${name}`;
+            const targetFilePathOg = `${targetPath}${name}`;
+            const targetFilePath = await avoidDuplicateName(sftp, targetFilePathOg);
+            // Copy file
+            await sftp.rcopy(sourceFilePath, targetFilePath);
+            await sftp.delete(sourceFilePath, true);
+          }
+          res.json({ cwd: { name: pathParam, type: 'folder' }, files: getDetails(sftp, targetPath, names) });
+          break;
+        }
+        case 'create': { // for now only mkdir is possible
+          const name = req.body.name;
+          const newFolderPath = `${pathParam}${name}/`;
+          // Create the folder
+          await sftp.mkdir(newFolderPath, true);
+          res.json({ cwd: { name: pathParam, type: 'folder' }, files: getDetails(sftp, newFolderPath) });
+          break;
+        }
+        case 'details': {
+          const names = req.body.names || [];
+          res.json({ cwd: { name: pathParam, type: 'folder' }, files: getDetails(sftp, pathParam, names) });
+          break;
+        }
+
+      }
+
       await sftp.end();
     } catch (error) {
       console.error('Error listing files:', error);
       res.status(500).send({ error: 'Failed to list files' });
     }
   });
+
+  async function avoidDuplicateName(sftp, targetFilePathOg) {
+
+    let targetFilePath = targetFilePathOg;
+    const parseFilePath = (filePath) => {
+      const fileName = path.basename(filePath, path.extname(filePath)); // Extract file name without extension
+      const fileExt = path.extname(filePath); // Extract extension
+      const dir = path.dirname(filePath); // Extract directory path
+      return {dir, fileName, fileExt};
+    };
+
+    const {dir, fileName, fileExt} = parseFilePath(targetFilePathOg);
+    let index = 1; // Start indexing from 1 for suffix
+
+    // Check if file exists and generate new target path
+    while (await sftp.exists(targetFilePath)) {
+      targetFilePath = `${dir}/${fileName}_${index}${fileExt}`;
+      index++;
+    }
+    return targetFilePath;
+  }
+  async function getDetails(sftp, path, names = undefined) {
+    const details = [];
+    if (!names) {
+      const fullPath = `${path}`;
+      const stats = await sftp.stat(fullPath);
+
+      details.push({
+        name,
+        type: stats.isDirectory ? 'folder' : 'file',
+        size: stats.size,
+        accessTime: stats.atime,
+        modifyTime: stats.mtime,
+        createTime: stats.birthtime || null, // birthtime may not always be available
+      });
+    } else {
+      for (const name of names) {
+        const fullPath = `${path}${name}`;
+        const stats = await sftp.stat(fullPath);
+
+        details.push({
+          name,
+          type: stats.isDirectory ? 'folder' : 'file',
+          size: stats.size,
+          accessTime: stats.atime,
+          modifyTime: stats.mtime,
+          createTime: stats.birthtime || null, // birthtime may not always be available
+        });
+      }
+    }
+    return details;
+  }
 
 // File Upload
   expressApp.post('/upload/:id', upload.single('uploadFiles'), async (req, res) => {
