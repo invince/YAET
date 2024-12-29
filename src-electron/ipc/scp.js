9a702c1a958bf105a2910e98beb33bdb6c734c29
invince <invincezero@gmail.com>
2024-12-29 20:02:47 +0100

    scp delete #89

--- src-electron/ipc/scp.js
+++ src-electron/ipc/scp.js
@@ -10,9 +10,9 @@ function initScpSftpHandler(scpMap, expressApp) {
     scpMap.set(id, config);
   });
 
+//==================== API ==========================================================
 
-
-  expressApp.post('/list/:id', async (req, res) => {
+  expressApp.post('/api/v1/scp/:id', async (req, res) => {
     const action = req.body.action || 'read';
     const pathParam = req.body.path || '/';
 
@@ -42,12 +42,14 @@ function initScpSftpHandler(scpMap, expressApp) {
         }
         case 'delete': {
           const names = req.body.names || [];
+          const details = [];
           for (let i = 0; i < names.length; i++) {
             const name = names[i];
             const fileAbsPath = `${pathParam}${name}`;
-            await sftp.delete(fileAbsPath, true);
+            details.push(getDetails(sftp, pathParam, name));
+            await sftp.delete(fileAbsPath);
           }
-          res.json({ cwd: { name: pathParam, type: 'folder' }});
+          res.json({ cwd: { name: pathParam, type: 'folder' }, files: details});
           break;
         }
         case 'rename': {
@@ -110,6 +112,75 @@ function initScpSftpHandler(scpMap, expressApp) {
     }
   });
 
+// File Upload
+  expressApp.post('/api/v1/scp/upload/:id', upload.single('uploadFiles'), async (req, res) => {
+    const {  data } = req.body;
+    console.log(req.body);
+    const path = JSON.parse(data).name; // path is incorrect on req.body
+    const configId = req.params['id'];
+    const config = scpMap.get(configId);
+    if (!config) {
+      console.error('Error connection config not found');
+      res.status(500).send({ error: 'Failed to list files' });
+    }
+    if (!req.file) {
+      console.error('Error: No file uploaded');
+      return res.status(400).send({ error: 'No file uploaded' });
+    }
+
+    try {
+      const sftp = new SftpClient();
+
+      // Connect to the SFTP server
+      await sftp.connect(config);
+
+      // Upload the file
+      const remotePath = avoidDuplicateName(sftp, `${path}/${req.file.originalname}`);
+      await sftp.put(req.file.buffer, remotePath);
+
+      res.json({ success: true, message: `File uploaded to ${remotePath}` });
+
+      // End the SFTP connection
+      await sftp.end();
+    } catch (error) {
+      console.error('Error uploading file:', error);
+      res.status(500).send({ error: 'Failed to upload file' });
+    }
+  });
+
+// File Download
+  expressApp.post('/api/v1/scp/download/:id', upload.none(), async (req, res) => {
+    const downloadInput = JSON.parse(req.body.downloadInput);
+    const path = downloadInput.path;
+    const names = downloadInput.names; // Assuming a single file download
+
+    console.log(req.body);
+    const configId = req.params['id'];
+    const config = scpMap.get(configId);
+    if (!config) {
+      console.error('Error connection config not found');
+      res.status(500).send({ error: 'Failed to list files' });
+    }
+    try {
+      const sftp = new SftpClient();
+      await sftp.connect(config);
+
+      for (const name of names) {
+        const fullPath = path + name;
+        const buffer = await sftp.get(fullPath);
+
+        res.set('Content-Disposition', `attachment; filename=${path.split('/').pop()}`);
+        res.send(buffer);
+      }
+
+      await sftp.end();
+    } catch (error) {
+      console.error('Error downloading file:', error);
+      res.status(500).send({ error: 'Failed to download file' });
+    }
+  });
+
+//================= Utils =================================================
   async function avoidDuplicateName(sftp, targetFilePathOg) {
 
     let targetFilePath = targetFilePathOg;
@@ -162,73 +233,6 @@ function initScpSftpHandler(scpMap, expressApp) {
     return details;
   }
 
-// File Upload
-  expressApp.post('/upload/:id', upload.single('uploadFiles'), async (req, res) => {
-    const {  data } = req.body;
-    console.log(req.body);
-    const path = JSON.parse(data).name; // path is incorrect on req.body
-    const configId = req.params['id'];
-    const config = scpMap.get(configId);
-    if (!config) {
-      console.error('Error connection config not found');
-      res.status(500).send({ error: 'Failed to list files' });
-    }
-    if (!req.file) {
-      console.error('Error: No file uploaded');
-      return res.status(400).send({ error: 'No file uploaded' });
-    }
-
-    try {
-      const sftp = new SftpClient();
-
-      // Connect to the SFTP server
-      await sftp.connect(config);
-
-      // Upload the file
-      const remotePath = `${path}/${req.file.originalname}`;
-      await sftp.put(req.file.buffer, remotePath);
-
-      res.json({ success: true, message: `File uploaded to ${remotePath}` });
-
-      // End the SFTP connection
-      await sftp.end();
-    } catch (error) {
-      console.error('Error uploading file:', error);
-      res.status(500).send({ error: 'Failed to upload file' });
-    }
-  });
-
-// File Download
-  expressApp.post('/download/:id', upload.none(), async (req, res) => {
-    const downloadInput = JSON.parse(req.body.downloadInput);
-    const path = downloadInput.path;
-    const names = downloadInput.names; // Assuming a single file download
-
-    console.log(req.body);
-    const configId = req.params['id'];
-    const config = scpMap.get(configId);
-    if (!config) {
-      console.error('Error connection config not found');
-      res.status(500).send({ error: 'Failed to list files' });
-    }
-    try {
-      const sftp = new SftpClient();
-      await sftp.connect(config);
-
-      for (const name of names) {
-        const fullPath = path + name;
-        const buffer = await sftp.get(fullPath);
-
-        res.set('Content-Disposition', `attachment; filename=${path.split('/').pop()}`);
-        res.send(buffer);
-      }
-
-      await sftp.end();
-    } catch (error) {
-      console.error('Error downloading file:', error);
-      res.status(500).send({ error: 'Failed to download file' });
-    }
-  });
 
 // Start API
   expressApp.listen(3000, () => console.log('API listening on port 3000'));
