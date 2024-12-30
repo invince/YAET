2b0f07667c4387bdec15568528b3c3a7ed984005
invince <invincezero@gmail.com>
2024-12-30 15:36:31 +0100

    scp init path setup #90

--- src/app/domain/profile/SSHProfile.ts
+++ src/app/domain/profile/SSHProfile.ts
@@ -4,6 +4,9 @@ export class SSHProfile {
   public host: string = '';
   public port: number = 22;
 
+  public initPath?: string;
+
+  public initCmd?: string;
   public authType?: AuthType;
   public login: string = '';
   public password: string = '';
