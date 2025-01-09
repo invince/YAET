7001026c34b6418445f838b26d2ef8a14e5c597b
invince <invincezero@gmail.com>
2025-01-09 08:39:04 +0100

    You need your won syncfusion license key

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,18 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import { registerLicense } from '@syncfusion/ej2-base';
+
+export class Compatibility {
+
+  static settings = '1.0.0';
+  static cloud = '1.0.0';
+  static secrets = '1.0.0';
+  static profiles = '1.0.0';
+}
+
+// Registering Syncfusion license key
+registerLicense('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
