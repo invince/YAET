5b6c0b77eed2284221266e8cbdc161e191088410
invince <invincezero@gmail.com>
2026-04-05 00:23:59 +0200

    migration primeng

---
+++ src/app/primeng.module.ts
@@ -0,0 +1,94 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+// PrimeNG Modules
+import {ButtonModule} from 'primeng/button';
+import {IconButtonModule} from 'primeng/iconbutton';
+import {InputTextModule} from 'primeng/inputtext';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import {DropdownModule} from 'primeng/dropdown';
+import {DialogModule} from 'primeng/dialog';
+import {DrawerModule} from 'primeng/drawer';
+import {TabViewModule} from 'primeng/tabview';
+import {ToolbarModule} from 'primeng/toolbar';
+import {ChipModule} from 'primeng/chip';
+import {ChipsModule} from 'primeng/chips';
+import {TreeModule} from 'primeng/tree';
+import {AccordionModule} from 'primeng/accordion';
+import {CheckboxModule} from 'primeng/checkbox';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {ToggleSwitchModule} from 'primeng/toggleswitch';
+import {ProgressSpinnerModule} from 'primeng/progressspinner';
+import {ProgressBarModule} from 'primeng/progressbar';
+import {TableModule} from 'primeng/table';
+import {ContextMenuModule} from 'primeng/contextmenu';
+import {TieredMenuModule} from 'primeng/tieredmenu';
+import {AutoCompleteModule} from 'primeng/autocomplete';
+import {ListboxModule} from 'primeng/listbox';
+import {ToastModule} from 'primeng/toast';
+import {DividerModule} from 'primeng/divider';
+import {MenuModule} from 'primeng/menu';
+import {BadgeModule} from 'primeng/badge';
+import {TooltipModule} from 'primeng/tooltip';
+import {FileUploadModule} from 'primeng/fileupload';
+import {CardModule} from 'primeng/card';
+import {SplitButtonModule} from 'primeng/splitbutton';
+import {MessageModule} from 'primeng/message';
+import {MessagesModule} from 'primeng/messages';
+import {OverlayPanelModule} from 'primeng/overlaypanel';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import {DynamicDialogModule} from 'primeng/dynamicdialog';
+import {ScrollPanelModule} from 'primeng/scrollpanel';
+import {StyleClassModule} from 'primeng/styleclass';
+import {RippleModule} from 'primeng/ripple';
+import {FocusTrapModule} from 'primeng/focustrap';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    ButtonModule,
+    IconButtonModule,
+    InputTextModule,
+    InputTextareaModule,
+    DropdownModule,
+    DialogModule,
+    DrawerModule,
+    TabViewModule,
+    ToolbarModule,
+    ChipModule,
+    ChipsModule,
+    TreeModule,
+    AccordionModule,
+    CheckboxModule,
+    RadioButtonModule,
+    ToggleSwitchModule,
+    ProgressSpinnerModule,
+    ProgressBarModule,
+    TableModule,
+    ContextMenuModule,
+    TieredMenuModule,
+    AutoCompleteModule,
+    ListboxModule,
+    ToastModule,
+    DividerModule,
+    MenuModule,
+    BadgeModule,
+    TooltipModule,
+    FileUploadModule,
+    CardModule,
+    SplitButtonModule,
+    MessageModule,
+    MessagesModule,
+    OverlayPanelModule,
+    ConfirmDialogModule,
+    DynamicDialogModule,
+    ScrollPanelModule,
+    StyleClassModule,
+    RippleModule,
+    FocusTrapModule,
+  ]
+})
+export class PrimeNGModule { }
